fbbb36bb67de5a65b637ea27d57003af373502c7
Zachary Murray <89foxlx@gmail.com>
2019-11-01 19:12:32 -0500

    some progress made?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
+    
   <div>
     <Navigation />
+    <Route path="/" component={Home}/>
+    <Route path="/" component={About}/>
+    <Route path="/" component={Contact}/>
   </div>
 );
 
 export default App;
+
+
+{/* <Route path="/" component={Home}/>
+    <Route path="/" component={About}/>
+    <Route path="/" component={Contact}/> */}
